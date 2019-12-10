64678ce2360e9c211ae4fe184c0384245e33dc3f
wangao <wangao@feiniu.com>
2019-12-10 11:12:48 +0800

    本地测试成功

--- src/main/java/me/zbl/fullstack/consts/DataConsts.java
+++ src/main/java/me/zbl/fullstack/consts/DataConsts.java
@@ -7,6 +7,6 @@ package me.zbl.fullstack.consts;
  */
 public class DataConsts {
 
-  public static final int USERNAME_MAX_LENGTH = 20;
-  public static final int PASSWORD_MAX_LENGTH = 15;
+    public static final int USERNAME_MAX_LENGTH = 20;
+    public static final int PASSWORD_MAX_LENGTH = 15;
 }
